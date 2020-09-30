242411e2051b48fec2d6d5ea9360d8df3ce6c94c
sampletext32 <sampletext32@bk.ru>
2020-09-30 21:24:52 +0300

    Implement Ekb V2

--- ParserEkb/Program.cs
+++ ParserEkb/Program.cs
@@ -12,11 +12,12 @@ namespace ParserEkb
             return new HtmlWeb().Load(url);
         }
 
-        static void Main(string[] args)
+        private static void V1()
         {
             for (int i = 0; i < 6; i++)
             {
-                string group = $"https://asktel.ru/ekaterinburg/shkola/page{i + 1}/";
+                var page = i != 0 ? $"page{i + 1}/" : "";
+                string group = $"https://asktel.ru/ekaterinburg/shkoly/{page}";
                 Console.WriteLine(group);
                 var document = LoadDoc(@group);
                 var aNodes = document.DocumentNode.SelectNodes("//div[@class='companyEmail']/a");
@@ -24,6 +25,33 @@ namespace ParserEkb
                 File.AppendAllLines("emails.txt", emails);
                 Console.WriteLine(string.Join(", ", emails));
             }
+        }
+
+        private static void V2()
+        {
+            for (int i = 0; i < 11; i++)
+            {
+                string group = $"http://obrazovanie66.ru/shkoly/page/{i + 1}/";
+                Console.WriteLine(group);
+                var document = LoadDoc(group);
+                var aNodes = document.DocumentNode.SelectNodes("//a[@class='school_link']");
+                var links = aNodes.Select(a => a.Attributes["href"].Value);
+                foreach (var link in links)
+                {
+                    Console.WriteLine("\t{0}", link);
+                    var doc = LoadDoc(link);
+                    var aNodeEmails =
+                        doc.DocumentNode.SelectNodes("//tr/td[@class='txtTableTitle' and text()='Почта']/../td[2]/a");
+
+                    var emails = aNodeEmails.Select(a => a.InnerText);
+                    File.AppendAllLines("emails.txt", emails);
+                    Console.WriteLine(string.Join(", ", emails));
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
         }
     }
 }
